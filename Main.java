47e06892198220c9d27fc359c0e7420fae926cb8
Ziyoda Muslimova <121525223+Ziyoda1988@users.noreply.github.com>
2023-02-22 18:02:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+class Main {
+  public static void main(String[] args) {
+   
+    printInfo("");
+
+
+    }
+
+
+    public static void printInfo(String str){
+
+        //implement here
+
+        String first = "Yi";
+        String last = "Oh";
+        char Y = first.charAt(0);
+        char O = last.charAt(0);
+        System.out.println( "The first name: Yi, The last name: Oh. The initials:" + Y + O);
+
+        String first2 = "Justin";
+        String last2 = "Timberlake";
+        char J = first2.charAt(0);
+        char T = last2.charAt(0);
+        System.out.println( "The first name: Justin, The last name: Timberlake. The initials:" + J + T);
+
+        String first3 = "Donald";
+        String last3 = "Duck";
+        char D = first3.charAt(0);
+        char d = last3.charAt(0);
+       System.out.println( "The first name: Donald, The last name: Duck. The initials:" + D + d);
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+Create a method that takes full name as a String parameter and prints out the first and last names and intials;
+
+ printInfo("Barack Obama") ->
+    The first name: Barack
+    The last name: Obama
+    The initials: BO
+
+printInfo("Telman Weber") ->
+    The first name: Telman
+    The last name: Weber
+    The initials: TW
+
+printInfo("Justin Timberlake") ->
+    The first name: Justin
+    The last name: Timberlake
+    The initials: JT
+
+printInfo("Joris Voorn") ->
+    The first name: Joris
+    The last name: Voorn
+    The initials: JV
+
